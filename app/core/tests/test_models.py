42521e2b94ff78777f54dd4d16ebff95446aa6f2
Tarek Lel <tarek.lel@farfetch.com>
2020-09-05 10:53:04 +0400

    Added feature to filter recipes

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -82,5 +82,5 @@ class ModelTests(TestCase):
         mock_uuid.return_value = uuid
         file_path = models.recipe_image_file_path(None, 'myimage.jpg')
 
-        exp_path = f'upload/recipe/{uuid}.jpg'
+        exp_path = f'uploads/recipe/{uuid}.jpg'
         self.assertEqual(file_path, exp_path)
